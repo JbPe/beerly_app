8632d86a8a007315630db2a2d9003237656c786c
Jb Pe <jbpetit1992@gmail.com>
2021-11-30 14:29:46 +0100

    ajout des steps requests

--- app_beerly.py
+++ app_beerly.py
@@ -6,6 +6,8 @@ import requests
 import numpy as np
 from PIL import Image
 import pickle
+import pandas as pd
+import json
 
 if __name__ == '__main__':
     main_bg = "beerly_wp.png"
@@ -36,16 +38,15 @@ if __name__ == '__main__':
     # identification de l'user
     placeholder = st.form('test', clear_on_submit=True)
     with placeholder:
-        username = st.text_input('Username')
+        username = st.text_input('Username','99996')
         password = st.text_input('Password')
-        st.form_submit_button('Login')
+        login = st.form_submit_button('Login')
 
     # Si l'identification est successful
     if password == 'test' and username != None:
         # Select a file
         st.write(f'# Hello {username}!')
-        st.write('## Please upload a menu')
-        img = st.file_uploader("Your menu", type=['jpg', 'png', 'jpeg'])
+        img = st.file_uploader("Please upload a menu", type=['jpg', 'png', 'jpeg'])
 
         if img != None:
 
@@ -62,36 +63,67 @@ if __name__ == '__main__':
             img_reshape = img.reshape(height*width*channel)
             img_enc = base64.b64encode(img_reshape)
 
-            # request to be sent to the api. Need a user_id
-            headers = {}
-            headers['Content-Type'] = 'application/json'
-            request_dict = {'image' : img_enc.decode('utf8').replace("'", '"'),
-                            'user' : username,
-                            'height' : height,
-                            'width' : width,
-                            'channel' : channel}
-
-            #post request
-            #requests.post('http://127.0.0.1:8000/predict',json.dumps(request_dict), headers = headers)
-
-
             # Sliders
             st.write('## Choose your parameters')
-            st.write("### Or let them as is if you don't have any preferences")
-            aroma = st.slider("Aroma",value=1.0, min_value=0.0, max_value=1.0, step=0.1)
-            st.write("The aroma of a beer originates from a number of sources, which essentially comes down to the malt, hops, yeast and any additional ingredients added during the brewing process.")
-            appearance = st.slider("Appearance", value=1.0, min_value=0.0, max_value=1.0, step=0.1)
-            st.write("##### ")
-            palate = st.slider("Palate", value=1.0, min_value=0.0, max_value=1.0, step=0.1)
-            st.write("##### ")
-            taste = st.slider("Taste", value=1.0, min_value=0.0, max_value=1.0, step=0.1)
-            st.write("##### ")
+            st.write("Or let them as is if you don't have any preferences")
+            with st.expander("Aroma"):
+                aroma = st.slider(
+                    "The aroma of a beer originates from a number of sources, which essentially comes down to the malt, hops, yeast and any additional ingredients added during the brewing process.",
+                    value=1.0,
+                    min_value=0.0,
+                    max_value=1.0,
+                    step=0.1)
+            with st.expander("Appearance"):
+                appearance = st.slider(
+                    "The visual characteristics that may be observed in a beer are colour, clarity, and nature of the head. Colour is usually imparted by the malts used, notably the adjunct malts added to darker beers, though other ingredients may contribute to the colour of some styles such as fruit beers.",
+                    value=1.0,
+                    min_value=0.0,
+                    max_value=1.0,
+                    step=0.1)
+            with st.expander("Palate"):
+                palate = st.slider(
+                    "Palate is typically defined as the sense of taste. In Ratebeer's system, it refers to mouthfeel and not taste or aroma.",
+                    value=1.0,
+                    min_value=0.0,
+                    max_value=1.0,
+                    step=0.1)
+            with st.expander("Taste"):
+                taste = st.slider(
+                    "The taste of beer primarily depends on its ingredients. ... Hops adds a trace of bitterness to beer that goes well with citrus and floral additives. Yeast gives a neutral, sugary taste to beer and emphasizes fruit and spicy notes in its aroma.",
+                    value=1.0,
+                    min_value=0.0,
+                    max_value=1.0,
+                    step=0.1)
 
             #Call API
-            call_api = st.button('Go get me the best beer !')
+            call_api = st.button('Go get me the best beer my little man !')
 
             #appeler l'API avec image_file + aroma + appearance + palate + taste + username
             if call_api:
+
+                # request to be sent to the api
+                headers = {}
+                headers['Content-Type'] = 'application/json'
+                request_dict = {
+                    'image': img_enc.decode('utf8').replace("'", '"'),
+                    'user': username,
+                    'height': height,
+                    'width': width,
+                    'channel': channel,
+                    'taste': taste,
+                    'appearance': appearance,
+                    'palate': palate,
+                    'aroma': aroma,
+                    'overall': 1,
+                    'content': 1
+                }
+
+                # requête et décodage
+                #req = requests.post('http://127.0.0.1:8000/predict',json.dumps(request_dict), headers = headers)
+
+                #st.write(req)
+
+
                 url = "http://my-json-server.typicode.com/rtavenar/fake_api/tasks"
 
                 # return un DataFramede X bières
@@ -99,6 +131,20 @@ if __name__ == '__main__':
                 contenu = reponse.json()
                 dataframe = st.dataframe(contenu)
 
-                # 1 trier le dictionnaire
+                # Contenu de la réponse JSON
+                # st.write(type(contenu))
+                # st.write(contenu)
 
-                # 2 sélectionner la meilleure bière
+                # 1ère ligne de la réponse JSON
+                # st.write(contenu[0]["title"])
+                # st.write(contenu[0]["id"])
+
+                # sélectionner la meilleure bière
+
+
+                # best_beer_name
+                # best_beer_rating
+
+                col1, col2 = st.columns(2)
+                col1.metric("Beer", contenu[0]["title"])
+                col2.metric("Estimated Rating", contenu[0]["id"])
