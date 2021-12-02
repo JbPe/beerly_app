88464a1eb64e7237b1753f7ab8ce5a223dc5535b
Jb Pe <jbpetit1992@gmail.com>
2021-12-02 11:04:38 +0100

    API running

--- app_beerly.py
+++ app_beerly.py
@@ -11,11 +11,20 @@ if __name__ == '__main__':
     main_bg = "beerly_wp.png"
     main_bg_ext = "png"
 
-    #test
-
-
     # Name of the page + icon
     st.set_page_config(page_title='Beerly', page_icon='🍻')
+
+
+    #test padding
+    padding = 0
+    st.markdown(f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {padding}rem;
+        padding-right: {padding}rem;
+        padding-left: {padding}rem;
+        padding-bottom: {padding}rem;
+    }} </style> """, unsafe_allow_html=True)
+
 
     # Hide left menu
     st.markdown("""
@@ -32,82 +41,98 @@ if __name__ == '__main__':
             background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});background-repeat: no-repeat
             }}
         </style>
-        """,
-                unsafe_allow_html=True)
+        """, unsafe_allow_html=True)
 
     # CSS Parameters
+
+    # H1
     st.markdown(
-        '<style>h1{ font-family: "American Typewriter", cursive; font-size: 50px; line-height: 60px; margin: 10px 0 20px; text-transform: uppercase; text-shadow: 2px 2px 0 #000, margin: 10px 0 24px; text-align: center; }</style>',
+        '<style>h1{ font-family: "Palatino"; font-size: 40px; line-height: 60px; margin: 10px 0 20px; text-align: center;text-transform: uppercase; }</style>',
         unsafe_allow_html=True)
+    # H2
     st.markdown(
-        '<style>h2{font-family: "Palatino" ;font-size: 30px; letter-spacing: -1px; text-transform: uppercase; text-shadow: 1px 1px 0 #000, margin: 10px 0 24px; text-align: center; line-height: 50px;}</style>',
+        '<style>h2{font-family: "Palatino" ;font-size: 30px; letter-spacing: -1px; text-shadow: 1px 1px 0 #000, margin: 10px 0 24px; line-height: 50px;}</style>',
         unsafe_allow_html=True)
+    # P
     st.markdown(
-        '<style>p{text-align: justify;font-family: "Palatino", sans-serif; font-size: 16px; line-height: 24px; margin: 0 0 24px;}</style>',
+        '<style>p{text-align: justify;font-family: "Palatino", sans-serif; font-size: 15px; line-height: 24px; margin: 0 0 24px;}</style>',
         unsafe_allow_html=True)
+    # H3
     st.markdown(
-        '<style>h3{font-family: "Impact" ;font-size: 30px; letter-spacing: -1px; text-transform: uppercase; text-shadow: 1px 1px 0 #000, margin: 10px 0 24px; text-align: center; line-height: 50px;}</style>',
+        '<style>h3{font-family: "Bookman" ;font-size: 25px; letter-spacing: -1px; text-shadow: 1px 1px 0 #000, margin: 10px 0 24px; text-align: center; line-height: 50px;}</style>',
+        unsafe_allow_html=True)
+    # H4
+    st.markdown(
+        '<style>h4{font-family: "Bookman" ;font-size: 20px; letter-spacing: -1px; text-shadow: 1px 1px 0 #000, margin: 10px 0 24px; text-align: center; line-height: 50px;}</style>',
         unsafe_allow_html=True)
 
     # user authentification
     placeholder = st.empty()
     with placeholder.form('test', clear_on_submit=True):
-        username = st.text_input('Username','Cadiz')
+        username = st.text_input('Username','89000')
         password = st.text_input('Password', type = 'password')
+        # centered button
         col1, col2, col3 , col4, col5 = st.columns(5)
         with col1:
             pass
         with col2:
             pass
+        with col3 :
+            login = st.form_submit_button('Login')
         with col4:
             pass
         with col5:
             pass
-        with col3 :
-            login = st.form_submit_button('Login')
 
     # If authentification is successful
     if password == 'test' and username != None:
+
+        #removing the authentification
         placeholder.empty()
+
         # Select a file
-        greetings = st.write(f'# Hello {username} !')
+        st.write(f'# Hello {username} !')
         st.write('## Please upload a menu')
-        img = st.file_uploader("\n", type=['jpg', 'png', 'jpeg'])
+        img = st.file_uploader("", type=['jpg', 'png', 'jpeg'])
 
         if img != None:
+
+            img.read()
+
             # Need to check if the image looks like a menu before calling the api
 
             # Sliders
-            st.write('## Choose your parameters')
-            st.write("Or let them as is if you don't have any preferences")
-            with st.expander("Aroma"):
+            with st.expander("Advanced parameters"):
                 aroma = st.slider(
-                    "The aroma of a beer originates from a number of sources, which essentially comes down to the malt, hops, yeast and any additional ingredients added during the brewing process.",
-                    value=1.0,
-                    min_value=0.0,
-                    max_value=1.0,
-                    step=0.1)
-            with st.expander("Appearance"):
+                    "Aroma",
+                    value=50,
+                    min_value=0,
+                    max_value=100,
+                    step=10, help="The aroma of a beer originates from a number of sources, which essentially comes down to the malt, hops, yeast and any additional ingredients added during the brewing process.")
                 appearance = st.slider(
-                    "The visual characteristics that may be observed in a beer are colour, clarity, and nature of the head. Colour is usually imparted by the malts used, notably the adjunct malts added to darker beers, though other ingredients may contribute to the colour of some styles such as fruit beers.",
-                    value=1.0,
-                    min_value=0.0,
-                    max_value=1.0,
-                    step=0.1)
-            with st.expander("Palate"):
+                    "Appearance",
+                    value=50,
+                    min_value=0,
+                    max_value=100,
+                    step=10,
+                    help="The visual characteristics that may be observed in a beer are colour, clarity, and nature of the head. Colour is usually imparted by the malts used, notably the adjunct malts added to darker beers, though other ingredients may contribute to the colour of some styles such as fruit beers."
+                )
                 palate = st.slider(
-                    "Palate is typically defined as the sense of taste. In Ratebeer's system, it refers to mouthfeel and not taste or aroma.",
-                    value=1.0,
-                    min_value=0.0,
-                    max_value=1.0,
-                    step=0.1)
-            with st.expander("Taste"):
+                    "Palate",
+                    value=50,
+                    min_value=0,
+                    max_value=100,
+                    step=10,
+                    help="Palate is typically defined as the sense of taste. In Ratebeer's system, it refers to mouthfeel and not taste or aroma."
+                )
                 taste = st.slider(
-                    "The taste of beer primarily depends on its ingredients. ... Hops adds a trace of bitterness to beer that goes well with citrus and floral additives. Yeast gives a neutral, sugary taste to beer and emphasizes fruit and spicy notes in its aroma.",
-                    value=1.0,
-                    min_value=0.0,
-                    max_value=1.0,
-                    step=0.1)
+                    "Taste",
+                    value=50,
+                    min_value=0,
+                    max_value=100,
+                    step=10,
+                    help="The taste of beer primarily depends on its ingredients. ... Hops adds a trace of bitterness to beer that goes well with citrus and floral additives. Yeast gives a neutral, sugary taste to beer and emphasizes fruit and spicy notes in its aroma."
+                )
 
             # convert image to np array
             img = Image.open(img)
@@ -121,18 +146,13 @@ if __name__ == '__main__':
             img_enc = base64.b64encode(img_reshape)
 
             #Call API
-            col1, col2, col3 , col4, col5 = st.columns(5)
+            col1, col2, col3 = st.columns(3)
             with col1:
                 pass
-            with col2:
+            with col3:
                 pass
-            with col4:
-                pass
-            with col5:
-                pass
-            with col3 :
+            with col2 :
                 call_api = st.button('Get me my beer !')
-
 
             #call the API with image_file + aroma + appearance + palate + taste + username
             if call_api:
@@ -146,33 +166,58 @@ if __name__ == '__main__':
                     'height': height,
                     'width': width,
                     'channel': channel,
-                    'taste': taste,
-                    'appearance': appearance,
-                    'palate': palate,
-                    'aroma': aroma,
+                    'taste': taste/50,
+                    'appearance': appearance/50,
+                    'palate': palate/50,
+                    'aroma': aroma/50,
                     'overall': 1,
                     'content': 5
                 }
 
                 # request & decoding
-                #req = requests.post('http://127.0.0.1:8000/predict',json.dumps(request_dict), headers = headers)
+                req = requests.post(
+                    'https://beerly0-kui5hxyjoa-ew.a.run.app/predict',
+                    json.dumps(request_dict),
+                    headers=headers)
 
-                #st.write(req)
+                content_response = req.json()
+
+                content_response = eval(content_response)
+
+                beer_list = []
+
+                for i in content_response['beer_name'].values():
+                    if i not in beer_list:
+                        beer_list.append(i)
 
 
-                url = "http://my-json-server.typicode.com/rtavenar/fake_api/tasks"
+                st.write(f'## You should try {beer_list[0]}!')
 
-                # return a dataframe of X beers
+                if len(beer_list) > 5:
+                    st.write(
+                        "Don't want this beer ? Look at the other beers in the top 5"
+                    )
+                    for i in range(1,5):
+                        st.write(beer_list[i])
+                else:
+                    st.write(
+                        "Don't want this beer ? Look at the other beers in the menu"
+                    )
+                    for i in range(1,len(beer_list)):
+                        st.write(beer_list[i])
 
-                #
-                response = requests.get(url, params="userId=3")
-                content_response = response.json()
 
-                content = pd.DataFrame(content_response)
+                #don't want this beer ? here is the top 5 of the menu for you
 
-                st.table(
-                    content.style.highlight_max(
-                        props='font-weight:bold;color:#FFFF00'))
+                # content = pd.DataFrame(content_response)
+
+                # st.write(f'## You should try {content_response[0]["title"]}')
+
+                # st.write(
+                #     '#### Here are the other beers of your menu ranked according to your taste :'
+                # )
+                # st.table(content)
+                #content.style.highlight_max(props='font-weight:bold;color:#FFFF00'))
 
                 # JSON Content
                 # st.write(type(contenu))
@@ -181,9 +226,3 @@ if __name__ == '__main__':
                 # 1st row of the JSON content
                 # st.write(contenu[0]["title"])
                 # st.write(contenu[0]["id"])
-
-                # Selecting the best beer
-
-                st.write("## The beer we've chosen for you is :")
-                st.write(f'### {content_response[0]["title"]}')
-                #contenu[0]["id"]
